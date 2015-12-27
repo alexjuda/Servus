8c548afede88d2efd4675716d9640d99774efffa
Alexander Juda <alexander.juda@estimote.com>
2015-12-27 10:51:45 +0100

    Initial Commit

---
+++ Servus/Servus.h
@@ -0,0 +1,19 @@
+//
+//  Servus.h
+//  Servus
+//
+//  Created by Alexander Juda on 27/12/15.
+//  Copyright © 2015 Alexander Juda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Servus.
+FOUNDATION_EXPORT double ServusVersionNumber;
+
+//! Project version string for Servus.
+FOUNDATION_EXPORT const unsigned char ServusVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Servus/PublicHeader.h>
+
+
